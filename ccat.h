974a2e4e3ac85263f79338ef15a201fffbfea7b8
Jennifer King <32186812+mairodia@users.noreply.github.com>
2018-04-05 15:58:14 -0700

    Update ccat.h

--- ccat.h
+++ ccat.h
@@ -5,23 +5,31 @@
 // Date: 4/3/2018
 // Project: CPet
 // Professor: Scott Edwards
-// Class: CSCI 123F | TR 9:30am
+// Class: CSCI 133F | TR 9:30am
 // Description:
 //      A header for CCat.
 // =================================================================================
-#include "cpet.h"
 #ifndef CCAT_H
 #define CCAT_H
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
 
 using namespace std;
 
-class CCat:public CPet
+class CCat
 {
     public:
-        CCat(const char *name):CPet(name){}
-        virtual void Eat(){cout << "I'm eating cat food!\n"; ++m_numMeals;}
-        virtual void Sleep(){cout << "Time for a long cat nap...\n"; ++m_numNaps;}
-}
+        CCat(const char *name): m_numMeals(0), m_numNaps(0){strcpy(m_name, name);}
+        void Eat(){cout << "I'm eating cat food!\n"; ++m_numMeals;}
+        void DispState() const;
+        void Sleep(){cout << "Time for a long cat nap...\n"; ++m_numNaps;}
+        void SayName() const{cout << m_name << ": ";}
+
+    private:
+        int m_numMeals;
+        int m_numNaps;
+        char m_name[50];
+};
 #endif
 
