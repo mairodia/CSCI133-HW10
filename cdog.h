0621a1bfc54d77e8a491a34712338d1df549cd3a
Jennifer King <32186812+mairodia@users.noreply.github.com>
2018-04-05 15:58:43 -0700

    Update cdog.h

--- cdog.h
+++ cdog.h
@@ -9,19 +9,27 @@
 // Description:
 //      A header for CDog.
 // =================================================================================
-#include "cpet.h"
 #ifndef CDOG_H
 #define CDOG_H
+#include <stdio.h>
+#include <string.h>
 #include <iostream>
 
 using namespace std;
 
-class CDog:public CPet
+class CDog
 {
     public:
-        CDog(const char *name):CPet(name){}
-        virtual void Eat(){cout << "I'm eating dog food!\n"; ++m_numMeals;}
-        virtual void Sleep(){cout << "Time for a doggy nap...\n"; ++m_numNaps;}
+        CDog(const char *name): m_numMeals(0), m_numNaps(0){strcpy(m_name, name);}
+        void Eat(){cout << "I'm eating dog food!\n"; ++m_numMeals;}
+        void DispState() const;
+        void Sleep(){cout << "Time for a doggy nap...\n"; ++m_numNaps;}
+        void SayName() const{cout << m_name << ": ";}
 
-}
+    private:
+        int m_numMeals;
+        int m_numNaps;
+        char m_name[50];
+
+};
 #endif
