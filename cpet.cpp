f2ec80cc5233d1eeec51fa784aea0deac07b38f5
Jennifer King <32186812+mairodia@users.noreply.github.com>
2018-04-05 15:59:05 -0700

    Update cpet.cpp

--- cpet.cpp
+++ cpet.cpp
@@ -9,8 +9,23 @@
 // Description:
 //  The class file for CPet.
 // =================================================================================
+#include "cpet.h"
+#include "cdog.h"
+#include "ccat.h"
 
-void CPet::DispSate() const
+void CPet::DispState() const
+{
+    cout << "So far, I have had " << m_numMeals << ((1 == m_numMeals)? " meal":" meals");
+    cout << "and " << m_numNaps << ((1 == m_numNaps)? " nap.":" naps." << endl;
+}
+
+void CCat::DispState() const
+{
+    cout << "So far, I have had " << m_numMeals << ((1 == m_numMeals)? " meal":" meals");
+    cout << "and " << m_numNaps << ((1 == m_numNaps)? " nap.":" naps." << endl;
+}
+
+void CDog::DispState() const
 {
     cout << "So far, I have had " << m_numMeals << ((1 == m_numMeals)? " meal":" meals");
     cout << "and " << m_numNaps << ((1 == m_numNaps)? " nap.":" naps." << endl;
