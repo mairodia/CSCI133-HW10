1a71030bd8dcbc9ff7e6de9716c2b59b2ee1788a
Jennifer King <32186812+mairodia@users.noreply.github.com>
2018-04-05 15:59:28 -0700

    Update cpet.h

--- cpet.h
+++ cpet.h
@@ -5,23 +5,30 @@
 // Date: 4/3/2018
 // Project: CPet
 // Professor: Scott Edwards
-// Class: CSCI 123F | TR 9:30am
+// Class: CSCI 133F | TR 9:30am
 // Description:
-//      A header for CPet.
+//       A header for CPet.
 // =================================================================================
-const int LEN = 50;
+#ifndef CPET_H
+#define CPET_H
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+
+using namespace std;
 
 class CPet
 {
     public:
         CPet(const char *name): m_numMeals(0), m_numNaps(0) {strcpy(m_name, name);}
-        virtual void Eat(){cout << "I'm eating generic pet food!\n"; ++m_numMeals;}
-        virtual void DispState() const;
-        virtual void Sleep(){cout << "CPet sleeping generically...\n"; ++m_numNaps;}
+        void Eat(){cout << "I'm eating generic pet food!\n"; ++m_numMeals;}
+        void DispState() const;
+        void Sleep(){cout << "CPet sleeping generically...\n"; ++m_numNaps;}
         void SayName() const {cout << m_name << ": ";}
 
     private:
         int m_numMeals;
         int m_numNaps;
-        char m_name[LEN];
-}
+        char m_name[50];
+};
+#endif
